2f490b3ee21bd9a935a239bbd5f8f95c1393e9ee
frankdj412 <frankdj412@gmail.com>
2015-07-15 10:43:29 +0800

    shell: Set default python as python2.

--- shell/lang.zsh
+++ shell/lang.zsh
@@ -1,15 +1,16 @@
 # c++
 export GTAGSLIBPATH=$HOME/.gtags/
 
-# Python
+# python
 
-# add path of tools into system path: nosetests
-# export PATH="/usr/local/share/python:$PATH"
-# export PYTHONPATH="/usr/local/lib/python2.7/site-packages/:${PYTHONPATH}"
-# export PYTHONPATH=" /usr/local/lib/python3.4/site-packages/:${PYTHONPATH}"
 alias virtualenv_activate="source ~/env/bin/activate"
 
 # Setting PATH for Python 2.7
 # The orginal version is saved in .bash_profile.pysave
 PATH="/Library/Frameworks/Python.framework/Versions/2.7/bin:${PATH}"
 export PATH
+
+# Setting PATH for Python 3.4
+# The orginal version is saved in .bash_profile.pysave
+# PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:${PATH}"
+# export PATH
